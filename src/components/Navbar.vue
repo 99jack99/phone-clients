d965e7a7325068db4c59cf038fecf4941aa1cf14
9jack9 <jackson55adl@gmail.com>
2025-02-02 16:39:51 +0100

    minor change

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,7 @@
     to="/"
     class="bg-black text-white py-5 flex align align-center justify-center text-xl font-semibold"
   >
-    Jackson Almeida - Clients test
+    Jackson Almeida - Cliently
   </RouterLink>
 </template>
 
